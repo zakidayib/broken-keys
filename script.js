93d0f72e263568f0e7955248158d90512efadbb2
zaki tayyip <zakidayib1997@gmail.com>
2023-06-14 17:19:07 +0300

    broken-keys_solved

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function brokenKeys(string1, string2) {
+  let brokenElement = [];
+  for (let i = 0; i < string1.length; i++) {
+    if (string2[i] !== string1[i]) {
+      brokenElement.push(string1[i]);
+    }
+  }
+
+  return brokenElement;
+}
+
+console.log(brokenKeys("good night", "gppd night"));
